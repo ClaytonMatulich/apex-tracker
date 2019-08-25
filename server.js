6c8a82c472e6ab6c3ab2de0d930bcf07b4bc1da2
Clayton Matulich <claytonmatulich13@gmail.com>
2019-08-25 15:50:42 -0700

    ready for deploy

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ if (process.env.NODE_ENV === "development") {
 //Profile routes
 app.use("/api/v1/profile", require("./routes/profile"));
 
+// Handle production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static(__dirname + "/public/"));
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
